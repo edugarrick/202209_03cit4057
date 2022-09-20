3caff15cab06cf68650d235c6b3c929ee75b918f
Liyuu-HKCT <s2255713@student.hkct.edu.hk>
2022-09-20 16:35:08 +0800

    Classwork-1.2.

--- cw01/src/22557138/sumsquare.py
+++ cw01/src/22557138/sumsquare.py
@@ -4,16 +4,18 @@ This program calculates the sum of
 square of first N natural
 numbers
 '''
+
 def squaresum (n):
     sum = 0
     for i in range(1, n+1 ) :
         sum = sum + (i * i )
     return sum
 
-def main( ) :
-    integerN =4
+def main() :
+    integerN = 4
     # Try line wrap within parentheses
-    print ( 'The sum of square of integer N is ',
-    squaresum ( integerN) )
-if __name__=="__main__":
-    main ( )
+    print ( "The sum of square of integer N is ",
+        squaresum ( integerN))
+
+if __name__ == "__main__":
+    main()
