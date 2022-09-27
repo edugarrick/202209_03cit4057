2c88116e1b1a0df5aa4427d826109c8b5f91b7ce
unknown <englishverygoodtheman@gmail.com>
2022-09-27 16:40:49 +0800

    lab01

---
+++ lab01/src/22563705/lab-632D287D-2.py
@@ -0,0 +1,18 @@
+from pickletools import string1
+
+
+def getString() :
+    ret = ""
+    # START: Write your code here
+    atr1 = "Holly"
+    atr2 = "Dolly"
+    ret = atr1+atr2
+
+    # END: Write you code here
+    return ret
+
+def main():
+    print( getString())
+
+if __name__ == "__main__":
+    main()
