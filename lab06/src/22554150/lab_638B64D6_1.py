d0b4134433aab7b3b33f9000bed4238ba82d204e
Elvificent <elvischu91041128@gmail.com>
2022-12-13 17:15:06 +0800

    Update lab_638B64D6_1.py

--- lab06/src/22554150/lab_638B64D6_1.py
+++ lab06/src/22554150/lab_638B64D6_1.py
@@ -3,19 +3,21 @@ class ClassManual:
     def __init__(self,name,leg_count): 
         self._name=name
         self._leg_count=leg_count
+        self.__dna = "ACGT"
     def eat(self): 
         print("{} is now eating".format(self._name))
     def walk(self): 
         print ("{} is now walking".format(self._name))
 
     def show_genome(self):
-
+        print("{} DNA: {}".format(self._name, self.__dna) )
     def __cellsplit(self):
 
 def main():
     obj_cat=ClassManual("Cat",4)
     obj_dog=ClassManual("Dog",4) 
     obj_human=ClassManual("Human",2) 
+    
     print("Mammal {} has {} legs.".format(
         obj_cat._name,obj_cat._leg_count)
     )
